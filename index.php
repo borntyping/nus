6e024427ef67fe4f098575264194b947d06da426
Sam Clements <sam@borntyping.co.uk>
2010-08-25 12:04:27 +0100

    Cleaned up, finished grab_gets()

--- index.php
+++ index.php
@@ -1,58 +1,38 @@
 <?php
 	// Site index
-	// Should be redirected to by the 404 ErrorDocument
-	// $dir = "/nus";
-	$getpage_style = 1;
-	$testing = 1;
 	
-	
-	function getpage_style_404($dir) {
-		// Get requested page
-		// Url should be formatted as www.domian.com/page?other
-		// Redirect should be though 404, i.e.
-		// ErrorDocument 404 /nus/index.php
-		$page = $_SERVER["REQUEST_URI"];
-		$page = explode('?',$page,2);
-		$page = $page[0];
-		$page = str_replace($dir, "", $page);
-		return $page;
-		unset ($page);
-	}
-	
-	function getpage_style_redirect() {
-		// Get requested page
-		// Url should be formatted as www.domian.com/page?other
-		// Redirect should be though server redirects
-		echo $_GET["page"];
+	function getpage() {
+		// Gets the name of the requested page
 		$page = $_GET["page"];
 		return $page;
-		unset ($page);
 	}
 	
 	function grab_gets() {
+		// Gets the $_GET values directly though the url, for use with includes
 		$gets = $_SERVER["REQUEST_URI"];
+		
 		$gets = explode('?',$gets,2);
+		if(!isset($gets[1])) :
+			return array();
+		endif;
 		$gets = $gets[1];
+		
 		$gets = explode('&',$gets);
 		foreach ($gets as &$get) :
 			$line = explode("=",$get);
 			$one = $line[0];
+			if(!isset($line[1]))
+				$line[1] = TRUE;
 			$two = $line[1];
 			$gets_final[$one] = $two;
 			unset($line);
 		endforeach;
 		return $gets_final;
-		unset($gets_final);
 	}
 	
-	echo "<b>".$_GET["page"]."</b><br>";
-	$page = $_GET["page"];
-	echo $_SERVER["REQUEST_URI"]."<br>".$_SERVER["QUERY_STRING"]."<br>".$_SERVER["SCRIPT_NAME"]."<br><br>";
-	// print_r($_SERVER);
+	$page = getpage();
 	
-	$ext = "html";
-	
-	if ($page == "/") {
+	if ($page == ""|$page == "/") {
 		$page = "home";
 	} else {
 		$bad  = array("..","@","<",">",";");
@@ -66,9 +46,9 @@
 		$ext = "php";
 	endif;
 	
-	// include("theme/header.php");
+	// Echo theme
+	
+	echo "<b>".$page."</b><br>";
+	echo $_SERVER["REQUEST_URI"]."<br>".$_SERVER["QUERY_STRING"]."<br>".$_SERVER["SCRIPT_NAME"]."<br><br>";
 	include("pages/${page}.${ext}");
-	// echo "</div>"; // Add this to sidebar.
-	// include("theme/sidebar.php");
-	// include("theme/footer.php");
 ?>
