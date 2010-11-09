b27d4528a6f2ecd935bf11cc152160129be273ec
Sam Clements <sam@borntyping.co.uk>
2010-11-09 12:57:38 +0000

    Cleanup of backup files.

--- app/failsafe.php
+++
@@ -1,6 +0,0 @@
-<?php
-$page->find_gets();
-echo "<b>404: Page Not Found</b>";
-if(isset($page->gets['search']))
-	echo '<br>You searched for <a href="/'.$page->gets['search'].'">'.$page->gets['search'].'</a>';
-echo '<br><br>Warning: 404 page not found. NUS-CMS reverted to failsafe.php';
--- pages/ip.php~
+++
@@ -1,1 +0,0 @@
-<?php echo $_SERVER["REMOTE_ADDR"]; ?>
