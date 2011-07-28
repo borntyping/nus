2a68a47b36b25938d62c71d261c347c547ee56e2
Sam Clements <sam@borntyping.co.uk>
2011-07-28 18:10:07 +0100

    Updated READMES

--- sites/default/pages/home.php
+++ sites/default/pages/home.php
@@ -1,26 +1,10 @@
-<p style="text-align:center;">
-	<b>Nice Urls, Simple CMS</b>
-	 - <a href="http://github.com/ziaix/nus">github.com/ziaix/nus</a>
-	 - &copy; <a href="http://borntyping.co.uk">Samuel Clements</a> 2010
-	</p>
+# Nice Urls, Simple CMS
 
-<p>If you can see this, NUS is probably working fine.</p>
+(github.com/ziaix/nus)[http://github.com/ziaix/nus]
 
-<p>
-	<b>To do:</b>
-	<ul>
-		<li>Replace this page with you homepage.</li>
-		<li>Add your theme to /themes/$themename/, and change
-			<pre>theme = ""</pre>
-			to
-			<pre>theme = "$themename"</pre>
-			inside <i>/app/config.ini</i>.
-		</li>
-		<li>If you're not running NUS in the top level directory, change
-			<pre>parentdir = ""</pre>
-			to
-			<pre>parentdir = "$dir/"</pre>
-			where $dir would be the directorys below /.
-		</li>
-	</ul>
-</p>
+&copy; (Samuel Clements)[http://borntyping.co.uk] 2011
+
+
+If you can see this, NUS is probably working fine.
+
+Check [/home] to make sure redirection is working.
